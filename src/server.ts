b036986bd453077fc7e8a1db6f69eaa509582e78
Rafi Ferdos <rafiferdos@gmail.com>
2026-06-03 23:42:26 +0600

    Initialize server to listen on configured port and start database

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
-import { databaseInitializer } from "./db"
+import app from './app'
+import config from './config'
+import { databaseInitializer } from './db'
 
 const main = () => {
   databaseInitializer()
+  app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`)
+  })
 }
+
+main()
