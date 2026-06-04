d45f4ff58e811e88a1937cac61783c6204bdd147
Rafi Ferdos <rafiferdos@gmail.com>
2026-06-04 16:50:44 +0600

    Refactor registerUser to use AppError for validation and improve error handling

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,18 +1,17 @@
 import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
+import AppError from '../../utils/AppError'
 import catchAsync from '../../utils/catchAsync'
 import sendResponse from '../../utils/sendResponse'
 import { authServices } from './auth.service'
 
 const registerUser = catchAsync(async (req: Request, res: Response) => {
   const { name, email, password } = req.body
-
   if (!name || !email || !password)
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      success: false,
-      message: 'Validation Error',
-      errors: 'Name, email, and password are strictly required.'
-    })
+    throw new AppError(
+      StatusCodes.BAD_REQUEST,
+      'Name, email, and password are strictly required.'
+    )
 
   const result = await authServices.register(req.body)
   sendResponse(res, {
@@ -22,3 +21,23 @@ const registerUser = catchAsync(async (req: Request, res: Response) => {
   })
 })
 
+const loginUser = catchAsync(async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  if (!email || !password)
+    throw new AppError(
+      StatusCodes.BAD_REQUEST,
+      'Email and password are strictly required.'
+    )
+
+  const result = await authServices.login(req.body)
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    message: 'Login successful',
+    data: result
+  })
+})
+
+export const AuthControllers = {
+  register: registerUser,
+  login: loginUser
+}
