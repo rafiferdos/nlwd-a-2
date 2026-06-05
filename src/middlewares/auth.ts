de06e406ab1fc1a91a7394e402abf9da0683160a
Rafi Ferdos <rafiferdos@gmail.com>
2026-06-05 11:55:21 +0600

    Refactor auth middleware to streamline token verification and user retrieval

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -11,30 +11,25 @@ const auth = (...roles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     // console.log(roles)
     // console.log(req.headers.authorization)
-    try {
-      const token = req.headers.authorization
-      if (!token) throw new AppError(StatusCodes.UNAUTHORIZED, 'Unauthorized')
+    const token = req.headers.authorization
+    if (!token) throw new AppError(StatusCodes.UNAUTHORIZED, 'Unauthorized')
 
-      const decoded = jwt.verify(token, config.jwt_secret) as JwtPayload
-      const userData = pool.query(
-        `
-          SELECT * FROM users WHERE email=$1
-
+    const decoded = jwt.verify(token, config.jwt_secret) as JwtPayload
+    const result = await pool.query(
+      `
+          SELECT * FROM users WHERE id=$1
         `,
-        [decoded.email]
-      )
-      if ((await userData).rows.length === 0)
-        throw new AppError(StatusCodes.NOT_FOUND, 'User not found')
+      [decoded.id]
+    )
+    if (result.rows.length === 0)
+      throw new AppError(StatusCodes.NOT_FOUND, 'User not found')
 
-      const user = (await userData).rows[0]
-      if (roles.length && !roles.includes(user.roles))
-        throw new AppError(StatusCodes.FORBIDDEN, 'Forbidden')
+    const user = result.rows[0]
+    if (roles.length && !roles.includes(user.role))
+      throw new AppError(StatusCodes.FORBIDDEN, 'Forbidden')
 
-      req.user = decoded
-      next()
-    } catch (error) {
-      next(error)
-    }
+    req.user = decoded
+    next()
   })
 }
 
